b25d7f1df9e371b0542f0044236ad0085363816e
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-07-14 16:26:54 +0930

    fixed construction of board size

--- Board.java
+++ Board.java
@@ -3,14 +3,14 @@ public class Board {
 	private static final int ROWS = 8;
 	private static final int COLUMNS = 8;
 	
-	Piece[][] board = new Piece[COLUMNS][COLUMNS];
+	Piece[][] board = new Piece[ROWS][COLUMNS];
 
 	public Board() {
 	}
 	public void reset() {
 		for (int i=0; i<8; i++) {
-			this.board[1][i] = new Rook();
-			this.board[6][i] = new Rook();
+			this.board[1][i] = new Rook(new Move[2], "White");
+			this.board[6][i] = new Rook(new Move[2], "Black");
 		}	
 	}
 	public boolean spotAvailable() {

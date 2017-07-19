536fb76f3c8fc047473ff3417dc491bfde8b3aa0
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-07-19 17:47:29 +0930

    Setup moves for pawn, now has special move, and chooses its direction based on it's colour

--- Pawn.java
+++ Pawn.java
@@ -1,16 +1,33 @@
 
 public class Pawn extends Piece {
-	private int[] directions = {1};
+	private int[] directions = new int[1];
 	private int maxDistance = 1;
 
 	public Pawn(Position position, String colour) {
 		super(position, colour, 1);
+		if (this.getColour().equals("White")) {
+			this.directions[0] = 1;
+		} else {
+			this.directions[0] = 5;
+		}
 	}
 	public int[] getDirections() {
 		return this.directions;
 	}
 	public int getMaxDistance() {
-		return this.maxDistance;
+		if (this.getColour().equals("White")) {
+			if (this.getPosition().getRow() == 2) {
+				return 2;
+			} else {
+				return 1;
+			}
+		} else {
+			if (this.getPosition().getRow() == 7) {
+				return 2;
+			} else {
+				return 1;
+			}
+		}
 	}
 	public String toString() {
 		if (this.getColour().equals("White")) {

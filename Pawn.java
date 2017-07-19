cbaea5407f4980858a8b192d1b8da5faea0f6e33
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-07-19 18:43:29 +0930

    Removed unused variable

--- Pawn.java
+++ Pawn.java
@@ -1,7 +1,6 @@
 
 public class Pawn extends Piece {
 	private int[] directions = new int[1];
-	private int maxDistance = 1;
 
 	public Pawn(Position position, String colour) {
 		super(position, colour, 1);

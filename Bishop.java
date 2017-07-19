378102809740b57858d06fd33a8545d8a79e5317
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-07-19 18:37:32 +0930

    Found the auto-formatting

--- Bishop.java
+++ Bishop.java
@@ -1,17 +1,20 @@
 
 public class Bishop extends Piece {
-	private int[] directions = {2, 4, 6, 8};
+	private int[] directions = { 2, 4, 6, 8 };
 	private int maxDistance = 7;
 
 	public Bishop(Position position, String colour) {
 		super(position, colour, 3);
 	}
+
 	public int[] getDirections() {
 		return this.directions;
 	}
+
 	public int getMaxDistance() {
 		return this.maxDistance;
 	}
+
 	public String toString() {
 		if (this.getColour().equals("White")) {
 			return "♗";
--- King.java
+++ King.java
@@ -1,17 +1,20 @@
 
 public class King extends Piece {
-	private int[] directions = {1, 2, 3, 4, 5, 6, 7, 8};
+	private int[] directions = { 1, 2, 3, 4, 5, 6, 7, 8 };
 	private int maxDistance = 1;
 
 	public King(Position position, String colour) {
 		super(position, colour, 100);
 	}
+
 	public int[] getDirections() {
 		return this.directions;
 	}
+
 	public int getMaxDistance() {
 		return this.maxDistance;
 	}
+
 	public String toString() {
 		if (this.getColour().equals("White")) {
 			return "♔";

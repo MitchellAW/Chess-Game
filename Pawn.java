193ae54ea1b8ea7dbd1899ec5c812001d99c0742
Mitch Woollatt <mitchwoollatt@gmail.com>
2017-07-20 13:52:16 +0930

    all moves displaying correctly now, pawn gains properties of queen when reaches end, but is not able to move like one yet

--- Pawn.java
+++ Pawn.java
@@ -1,30 +1,114 @@
 
 public class Pawn extends Piece {
-	private int[] directions = new int[1];
+	private int[] directions = new int[3];
+	private int maxDistance = 1;
 
 	public Pawn(Position position, String colour) {
 		super(position, colour, 1);
 		if (this.getColour().equals("White")) {
-			this.directions[0] = 1;
+			this.directions[0] = 8;
+			this.directions[1] = 1;
+			this.directions[2] = 2;
 		} else {
-			this.directions[0] = 5;
+			this.directions[0] = 4;
+			this.directions[1] = 5;
+			this.directions[2] = 6;
 		}
 	}
 
 	public int[] getDirections() {
+		if (this.getColour().equals("White")) {
+			if (this.getPosition().getRow() == 8) {
+				this.directions = new int[8];
+				for (int i = 0; i < 8; i++) {
+					this.directions[i] = i+1;
+				}
+				return this.directions;
+			}
+		}
 		return this.directions;
+	}
+
+	public Position[] getMoves(Board board) {
+		Position[] moves = new Position[countMoves(board)];
+		Position curr = this.getPosition();
+		int index = 0;
+
+		for (int i = 0; i < directions.length; i++) {
+			if (i % 2 == 0) {
+				if (curr.positionAt(this.directions[i], 1).isValid()) {
+					Object pieceAt = board.getPieceAt(curr.positionAt(directions[i], 1));
+					if (pieceAt instanceof Piece) {
+						if (((Piece)pieceAt).getColour() != this.getColour())  {
+							moves[index] = curr.positionAt(directions[i], 1);
+							index++;
+						}
+					}
+				}
+			} else {
+				if (curr.positionAt(this.directions[i], 1).isValid()) {
+					for (int j = 1; j <= this.getMaxDistance(); j++) {
+						Object pieceAt = board.getPieceAt(curr.positionAt(directions[i], j));
+						if (pieceAt instanceof Piece) {
+							break;
+						} else {
+							moves[index] = curr.positionAt(directions[i], j);
+							index++;
+						}
+					}
+				}
+			}
+		}
+
+		return moves;
+	}
+
+	public int countMoves(Board board) {
+		int moveCount = 0;
+		Position curr = this.getPosition();
+		
+		for (int i = 0; i < directions.length; i++) {
+			if (i % 2 == 0) {
+				if (curr.positionAt(this.directions[i], 1).isValid()) {
+					Object pieceAt = board.getPieceAt(curr.positionAt(directions[i], 1));
+					if (pieceAt instanceof Piece) {
+						if (((Piece)pieceAt).getColour() != this.getColour())  {
+							moveCount++;
+						}
+					}
+				}
+			} else {
+				if (curr.positionAt(this.directions[i], 1).isValid()) {
+					for (int j = 1; j <= this.getMaxDistance(); j++) {
+						Object pieceAt = board.getPieceAt(curr.positionAt(directions[i], j));
+						if (pieceAt instanceof Piece) {
+							break;
+						} else {
+							moveCount++;
+						}
+					}
+				}
+			}
+		}
+		return moveCount;
 	}
 
 	public int getMaxDistance() {
 		if (this.getColour().equals("White")) {
-			if (this.getPosition().getRow() == 2) {
+			if (this.getPosition().getRow() == 2 && this.maxDistance == 1) {
 				return 2;
+			} else if (this.getPosition().getRow() == 8) {
+				this.maxDistance = 7;
+				return this.maxDistance;
 			} else {
 				return 1;
 			}
 		} else {
-			if (this.getPosition().getRow() == 7) {
+			if (this.getPosition().getRow() == 7 && this.maxDistance == 1) {
 				return 2;
+			} else if (this.getPosition().getRow() == 1) {
+				this.maxDistance = 7;
+				return this.maxDistance;
 			} else {
 				return 1;
 			}

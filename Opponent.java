ceb20f03289eff2e375d8107876625548b6335c0
Mitch Woollatt <mitchwoollatt@gmail.com>
2017-07-22 11:39:40 +0930

    Added some basic checks in the computer move before choosing a random one

--- Opponent.java
+++ Opponent.java
@@ -11,16 +11,61 @@ public class Opponent {
 
 	public Position[] getRandomMove(Board board) {
 
-		Position[] fromTo = new Position[2];
-
+		Position[] bestMove = new Position[2];
 		Position[][] allMoves = board.getAllLegalMoves(this.colour);
 
+		for (int i = 0; i < allMoves[0].length; i++) {
+			board.move(allMoves[0][i], allMoves[1][i]);
+			// If a move will checkmate the opponent, take it
+			if (board.isCheckmate(getOppositeColour())) {
+				board.undo();
+				bestMove[0] = allMoves[0][i];
+				bestMove[1] = allMoves[1][i];
+//				System.out.println("Checkmate");
+				return bestMove;
+			}
+			board.undo();
+		}
+
+		for (int i = 0; i < allMoves[0].length; i++) {
+			board.move(allMoves[0][i], allMoves[1][i]);
+			// If a move will check the opponent, take it
+			if (board.isCheck(getOppositeColour())) {
+				board.undo();
+				bestMove[0] = allMoves[0][i];
+				bestMove[1] = allMoves[1][i];
+//				System.out.println("Check");
+				return bestMove;
+			}
+			board.undo();
+		}
+
+		int highestValue = 0;
+
+		// Take the piece of highest value
+		for (int i = 0; i < allMoves[0].length; i++) {
+			if (board.getColourAt(allMoves[1][i]).equals(getOppositeColour())) {
+				Piece piece = ((Piece)board.getPieceAt(allMoves[1][i]));
+				if (piece.getValue() > highestValue) {
+					highestValue = piece.getValue();
+					bestMove[0] = allMoves[0][i];
+					bestMove[1] = allMoves[1][i];
+//					System.out.println("Take piece");
+				}
+			}
+		}
+
+		if (highestValue > 0) {
+			return bestMove;
+		}
+
+		// Make a random move
 		int choice = rand.nextInt(allMoves[0].length);
 
-		fromTo[0] = allMoves[0][choice];
-		fromTo[1] = allMoves[1][choice];
+		bestMove[0] = allMoves[0][choice];
+		bestMove[1] = allMoves[1][choice];
 
-		return fromTo;
+		return bestMove;
 	}
 
 	public int countPieces(Board board) {
@@ -57,5 +102,13 @@ public class Opponent {
 			}
 		}
 		return pieces;
+	}
+
+	public String getOppositeColour() {
+		if (this.colour == "Black") {
+			return "White";
+		} else {
+			return "Black";
+		}
 	}
 }

3070ee1ae4319f3d963fff07ce36d0e7350ed401
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-11-20 10:04:58 +1030

    Refactored knights getMoves() method

--- pieces/Knight.java
+++ pieces/Knight.java
@@ -1,4 +1,5 @@
 package pieces;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class Knight extends Piece {
 	public Knight(Position position, String colour) {
 		super(position, colour, 3);
 	}
-	
+
 	public Knight(Knight other) {
 		super(other);
 	}
@@ -21,21 +22,21 @@ public class Knight extends Piece {
 	// Get all possible (valid) moves
 	public List<Move> getMoves(Board board) {
 		List<Move> moves = new ArrayList<Move>();
-		Position curr = this.getPosition();
+		Position startPos = this.getPosition();
 
 		for (int i = 1; i < directions.length; i += 2) {
 			for (int j = -1; j < 2; j += 2) {
-				// Move north, east, south or west
-				Position newPos = curr.positionAt(directions[i], 1);
-				if (newPos.positionAt(directions[i + j], 1).isValid()) {
-					Position newPos2 = newPos.positionAt(directions[i + j], 1);
-					Object pieceAt = board.getPieceAt(newPos2);
-					if (pieceAt instanceof Piece) {
-						if (((Piece) pieceAt).getColour() != this.getColour()) {
-							moves.add(new Move(board, curr, newPos2));
-						}
-					} else {
-						moves.add(new Move(board, curr, newPos2));
+				// Perform first half of knight movement
+				Position halfPos = startPos.positionAt(directions[i], 1);
+				if (halfPos.positionAt(directions[i + j], 1).isValid()) {
+					// Perform second half of knight movement
+					Position endPos = halfPos.positionAt(directions[i + j], 1);
+					Piece pieceTaken = board.getPieceAt(endPos);
+					if (pieceTaken != null
+							&& pieceTaken.getColour() != this.getColour()) {
+						moves.add(new Move(board, startPos, endPos));
+					} else if (pieceTaken == null) {
+						moves.add(new Move(board, startPos, endPos));
 					}
 				}
 			}
